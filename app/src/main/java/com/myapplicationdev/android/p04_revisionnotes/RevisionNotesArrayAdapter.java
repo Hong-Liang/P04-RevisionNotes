70437a1658ec546fa17eb1d28a59ff7022fd4eaa
Hong-Liang <57203229+Hong-Liang@users.noreply.github.com>
2020-05-14 13:39:47 +0800

    Final Commit

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
@@ -11,41 +11,62 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
-	Context context;
-	ArrayList<Note> notes;
-	int resource;
-	ImageView iv1, iv2, iv3, iv4, iv5;
+    Context context;
+    ArrayList<Note> notes;
+    int resource;
+    ImageView iv1, iv2, iv3, iv4, iv5;
+    TextView tvNote;
 
-	public RevisionNotesArrayAdapter(Context context, int resource, ArrayList<Note> notes) {
-		super(context, resource, notes);
-		this.context = context;
-		this.notes = notes;
-		this.resource = resource;
-	}
+    public RevisionNotesArrayAdapter(Context context, int resource, ArrayList<Note> notes) {
+        super(context, resource, notes);
+        this.context = context;
+        this.notes = notes;
+        this.resource = resource;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		View rowView = inflater.inflate(resource, parent, false);
+        View rowView = inflater.inflate(resource, parent, false);
 
-		//Match the UI components with Java variables
+        //Match the UI components with Java variables
+        iv1 = rowView.findViewById(R.id.imageView1star);
+        iv2 = rowView.findViewById(R.id.imageView2star);
+        iv3 = rowView.findViewById(R.id.imageView3star);
+        iv4 = rowView.findViewById(R.id.imageView4star);
+        iv5 = rowView.findViewById(R.id.imageView5star);
+        tvNote = rowView.findViewById(R.id.textViewNote);
 
-		Note note = notes.get(position);
+        Note note = notes.get(position);
+		int stars = note.getStars();
+        //Check if the property for starts >= 5, if so, "light" up the stars
+        tvNote.setText(note.getNoteContent());
 
-		//Check if the property for starts >= 5, if so, "light" up the stars
-		if (/*stars >= 5*/) {
-			iv5.setImageResource(android.R.drawable.btn_star_big_on);
-			iv4.setImageResource(android.R.drawable.btn_star_big_on);
-			iv3.setImageResource(android.R.drawable.btn_star_big_on);
-			iv2.setImageResource(android.R.drawable.btn_star_big_on);
-			iv1.setImageResource(android.R.drawable.btn_star_big_on);
-		}
-
-		return rowView;
-	}
-
+        if (stars >= 5) {
+            iv5.setImageResource(android.R.drawable.btn_star_big_on);
+            iv4.setImageResource(android.R.drawable.btn_star_big_on);
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (stars == 4) {
+            iv4.setImageResource(android.R.drawable.btn_star_big_on);
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        }  else if (stars == 3) {
+            iv3.setImageResource(android.R.drawable.btn_star_big_on);
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else if (stars == 2) {
+            iv2.setImageResource(android.R.drawable.btn_star_big_on);
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        } else {
+            iv1.setImageResource(android.R.drawable.btn_star_big_on);
+        }
+        return rowView;
+    }
 
 
 }
